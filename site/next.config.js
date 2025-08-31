4589ef7aea715f40d9977b9cac83500b18ee2f54
X40c <142095995+X40c@users.noreply.github.com>
2025-08-31 19:23:28 +0000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'X40c\'s Registry',
+    description: 'Pen testing and security research focused images',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://x40c.github.io/custom-kasm-images',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/custom-kasm-images/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
